d21b40ce3b62d8cb804c2e6999f42c7afb8b3a34
starlc_2588@163.com <08@dyjd1219>
2025-03-07 00:07:24 +0800

    添加初始版本

---
+++ starlc-system/starlc-system-web/src/main/java/com/starlc/system/web/SystemWebApplication.java
@@ -0,0 +1,11 @@
+package com.starlc.system.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SystemWebApplication.class, args);
+    }
+}
